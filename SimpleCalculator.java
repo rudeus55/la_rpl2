04ca0ece9093f4513a3b5b7bc637d5a0c8545b84
rudeus55 <152457880+rudeus55@users.noreply.github.com>
2023-12-06 16:58:46 +0700

    Add files via upload

---
+++ SimpleCalculator.java
@@ -0,0 +1,46 @@
+package rpl;
+
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan angka pertama: ");
+        double angka1 = scanner.nextDouble();
+
+        System.out.print("Masukkan operasi (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.print("Masukkan angka kedua: ");
+        double angka2 = scanner.nextDouble();
+
+        double hasil = 0;
+
+        switch (operator) {
+            case '+':
+                hasil = angka1 + angka2;
+                break;
+            case '-':
+                hasil = angka1 - angka2;
+                break;
+            case '*':
+                hasil = angka1 * angka2;
+                break;
+            case '/':
+                if (angka2 != 0) {
+                    hasil = angka1 / angka2;
+                } else {
+                    System.out.println("Error: Pembagian oleh nol tidak diizinkan.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Error: Operator tidak valid.");
+                return;
+        }
+
+        System.out.println("Hasil: " + hasil);
+    }
+}
